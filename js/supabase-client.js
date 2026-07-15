0ad89ac0441d20b9b5fa532983e9681d880bff14
poissonpasnee <ifsiapp.dental168@passmail.com>
2026-07-15 17:03:08 +0200

    Update supabase-client.js

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -4,8 +4,8 @@
 (function (global) {
   "use strict";
 
-  const SUPABASE_URL = https://ktilbvibboqvkujstzed.supabase.co/rest/v1/;
-  const SUPABASE_ANON_KEY = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt0aWxidmliYm9xdmt1anN0emVkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQxMTk0MDcsImV4cCI6MjA5OTY5NTQwN30.aLo0E_uM6a7rEl9t2sEBPY_gZalXDwmDI_QSkhHTMqs;
+  const SUPABASE_URL = "https://ktilbvibboqvkujstzed.supabase.co/rest/v1/";
+  const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt0aWxidmliYm9xdmt1anN0emVkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQxMTk0MDcsImV4cCI6MjA5OTY5NTQwN30.aLo0E_uM6a7rEl9t2sEBPY_gZalXDwmDI_QSkhHTMqs";
 
   if (!global.supabase) {
     console.error("Le SDK Supabase (supabase-js) n'est pas chargé. Vérifie la balise <script> dans index.html.");
